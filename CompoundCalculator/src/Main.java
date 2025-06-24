6b54b610bd8281f4cb3e3dafe5e0356f6bfe4d8e
Shyanne Cortes <smc9253@g.rit.edu>
2025-06-24 18:06:58 -0400

    first working version of compound calcualtor using command line

---
+++ CompoundCalculator/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Compound interest calculator
+
+        Scanner scanner = new Scanner(System.in);
+
+        double principal;
+        double rate;
+        int timesCompounded;
+        int years;
+        double amount;
+
+        System.out.print("Enter the principal amount: ");
+        principal = scanner.nextDouble();
+
+        System.out.print("Enter the interest rate (in %): ");
+        rate = scanner.nextDouble() / 100;
+
+        System.out.print("Enter the number of times compounded per year: ");
+        timesCompounded = scanner.nextInt();
+
+        System.out.print("Enter the number of years: ");
+        years = scanner.nextInt();
+
+        amount = principal * Math.pow(1 + rate / timesCompounded, timesCompounded * years);
+
+        System.out.printf("The amount after %d year(s) is $ %.2f", years, amount);
+
+        scanner.close();
+
+    }
+}
+
